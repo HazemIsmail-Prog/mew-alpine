c46a8de87dfd06563a11bdb5163f66a1dbfc54de
Hazem <hazem.ismail@hotmail.com>
2024-12-02 06:36:36 +0300

    apply arrow selection in dropdown

--- resources/views/components/dropdown-test.blade.php
+++ resources/views/components/dropdown-test.blade.php
@@ -5,12 +5,17 @@
     expanded: false,
     searchTerm: '',
     selectedItems: {{ $selectedItems }},
+    focusedItemIndex: null,
     list: @js($list),
     init() {
         this.$watch('expanded', value => {
             if (value) {
                 this.updateListWidth();
-                this.$nextTick(() => this.$refs.searchTermRef.focus());
+                this.focusedItemIndex = null;
+                this.$nextTick(() => {
+                    this.$refs.searchTermRef.focus()
+                    this.$refs.scrollContainer.scrollTop = 0;                
+                });
             }
         });
     },
@@ -49,12 +54,67 @@
             this.selectedItems = item.id;
             this.expanded = false;
         }
-        this.$nextTick(() => this.updateListWidth());
+        this.$nextTick(() => {
+        this.updateListWidth();
+        this.searchTerm = '';
+        });
     },
     isSelected(item) {
         return this.multiple ?
             this.selectedItems?.includes(item.id) :
             this.selectedItems === item.id;
+    },
+    handleKeydown(e) {
+        if (!this.expanded) return;
+
+        if (e.key === 'ArrowDown') {
+            e.preventDefault();
+            if (this.focusedItemIndex === null) {
+                this.focusedItemIndex = 0; // Start at the first item
+                this.scrollToFocusedItem();
+            } else if (this.focusedItemIndex < this.sortedItems.length - 1) {
+                this.focusedItemIndex++; // Move down if not at the last item
+                this.scrollToFocusedItem();
+            }
+
+        } else if (e.key === 'ArrowUp') {
+            e.preventDefault();
+            if (this.focusedItemIndex === null) {
+                this.focusedItemIndex = 0; // Start at the first item
+                this.scrollToFocusedItem();
+            } else if (this.focusedItemIndex > 0) {
+                this.focusedItemIndex--; // Move up if not at the first item
+                this.scrollToFocusedItem();
+            }
+
+        } else if (e.key === 'Enter') {
+            e.preventDefault();
+            if (this.focusedItemIndex !== null) {
+                const item = this.sortedItems[this.focusedItemIndex];
+                this.addItem(item);
+            }
+        }
+    },
+    scrollToFocusedItem() {
+        this.$nextTick(() => {
+            const scrollContainer = this.$refs.scrollContainer;
+            const focusedItem = document.querySelector('.focused'); // Select the focused item
+
+            if (scrollContainer && focusedItem) {
+                const containerTop = scrollContainer.scrollTop;
+                const containerHeight = scrollContainer.offsetHeight;
+                const itemTop = focusedItem.offsetTop;
+                const itemHeight = focusedItem.offsetHeight;
+
+                if ((itemTop + itemHeight) > containerHeight) {
+                    scrollContainer.scrollTop = itemTop - (containerHeight - itemHeight);
+                }
+
+                if (containerTop < itemHeight) {
+                    scrollContainer.scrollTop = itemTop - (containerHeight - itemHeight) + itemHeight - containerTop;
+                }
+            }
+        });
     },
 }" x-ref="button" class="w-full overflow-hidden" x-model="selectedItems"
     x-modelable="{{ $model }}">
@@ -81,11 +141,17 @@
 
         <div x-cloak x-anchor.bottom-start-end="$refs.button" x-show="expanded" x-ref="list"
             class="z-20 bg-white border border-primary rounded-lg shadow-md overflow-clip">
-            <input class="w-full p-2 border-none rounded-md focus:outline-none focus:ring-0 outline-none" type="text"
+            <input x-on:keydown="handleKeydown($event)"
+                class="w-full p-2 border-none rounded-md focus:outline-none focus:ring-0 outline-none" type="text"
                 x-model="searchTerm" x-ref="searchTermRef" />
-            <div tabindex="-1" class="bg-white border-t border-primary p-2 max-h-[200px] overflow-y-auto no-scrollbar">
-                <template x-for="item in sortedItems" :key="item.id">
-                    <div x-bind:class="isSelected(item) ? 'bg-blue-200' : 'hover:bg-primary hover:bg-opacity-10'"
+            {{-- want this div to scroll to focused item --}}
+            <div x-ref="scrollContainer" tabindex="-1"
+                class="bg-white border-t border-primary p-2 max-h-[300px] overflow-y-auto no-scrollbar">
+                <template x-for="(item, index) in sortedItems" :key="item.id">
+                    <div x-bind:class="[
+                        isSelected(item) ? 'bg-blue-200' : 'hover:bg-primary hover:bg-opacity-10',
+                        focusedItemIndex === index && expanded ? 'focused bg-primary text-white rounded-md px-2 shadow-lg' : ''
+                    ]"
                         x-on:click="addItem(item)" class="select-none cursor-pointer p-1" x-text="item.name"></div>
                 </template>
             </div>
