1c3971860472438e1f58c9857ae490d37e3a5a83
Hazem <hazem.ismail@hotmail.com>
2024-12-13 17:39:01 +0300

    change print letters to normal view

--- app/Http/Controllers/LetterController.php
+++ app/Http/Controllers/LetterController.php
@@ -6,7 +6,6 @@ use App\Http\Resources\LetterResource;
 use App\Models\Letter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Mpdf\Mpdf;
 
 
 class LetterController extends Controller
@@ -90,52 +89,13 @@ class LetterController extends Controller
 
     public function originalPDF(Letter $letter)
     {
-        // $mpdf = new Mpdf([
-        //     'margin_bottom'            => 26,
-        //     'margin_header'            => 0,
-        //     'margin_footer'            => 0,
-        // ]);
-
-
-        $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
-        $fontDirs = $defaultConfig['fontDir'];
-
-        $defaultFontConfig = (new \Mpdf\Config\FontVariables())->getDefaults();
-        $fontData = $defaultFontConfig['fontdata'];
-
-        // Define your custom fonts directory and fonts
-        $mpdf = new \Mpdf\Mpdf([
-            'fontDir' => array_merge($fontDirs, [
-                public_path('fonts'), // Replace with your custom fonts directory
-            ]),
-            'fontdata' => $fontData + [
-                'sultan' => [
-                    'R' => 'Sultan4-Regular.ttf', // Regular font file
-                    // 'B' => 'Sultan2-Regular.ttf',   // Bold font file
-                    'useOTL' => 0xFF,
-                    'useKashida' => 75,
-                ],
-            ],
-            'default_font' => 'sultan', // Set the default font if needed
-        ]);
-
-        // $mpdf->autoLangToFont = true;
-
-        $mpdf->SetMargins(19, 19, 50);
-        $mpdf->SetWatermarkImage(public_path('images/cover.jpeg'));
-        $mpdf->showWatermarkImage = true;
-        $mpdf->watermarkImageAlpha = 1;
-        $mpdf->watermark_pos = 'P';
-        $mpdf->watermark_size = 'D';
-        $body = view('pages.letters.original-pdf', ['letter' => $letter]);
-        $mpdf->WriteHTML($body); //should be before output directly
-        $mpdf->Output('file_name.pdf', 'I');
+        $cover = asset('images/cover.jpeg');
+        return view('pages.letters.print', compact('letter', 'cover'));
     }
 
     public function normalPDF(Letter $letter)
     {
-        return Pdf::view('pages.letters.normal-pdf', ['letter' => $letter])
-            ->format('a4')
-            ->name('your-invoice.pdf');
+        $cover = false;
+        return view('pages.letters.print', compact('letter', 'cover'));
     }
 }
--- resources/views/pages/letters/print.blade.php
+++ resources/views/pages/letters/print.blade.php
@@ -30,36 +30,29 @@
             font-family: "Sultan Normal", sans-serif;
         }
 
-
-
-        /* Set page margins */
         @page {
-            margin-top: 7rem;
-            margin-bottom: 7rem;
+            size: A4 portrait;
+            margin: 7rem 0;
         }
 
         @page:first {
             margin: 0;
-            padding-bottom: 7rem;
-            /* background: url('data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/cover.jpeg'))) }}') no-repeat; */
-            /* background-size: 100%; */
-
+            margin-bottom: 7rem;
+            background-image: url({{ $cover }});
+            background-size: 100%;
+            background-repeat: no-repeat;
         }
 
-        /* Ensure content respects page margins */
-        .content-wrapper {
-            /* padding: 20mm; */
-            /* Add padding to match @page margins */
-            box-sizing: border-box;
-            min-height: calc(100vh - 12rem);
-
-        }
+        /*  */
     </style>
+
 </head>
 
-<body>
+<body class=" flex flex-col min-h-dvh w-[800px] mx-auto">
+
     <div class="mt-48"></div>
-    <div class="content-wrapper flex flex-col justify-between mx-20">
+
+    <div class="content-wrapper justify-between mx-20">
         @if ($letter->internal)
             <p class="text-center text-2xl font-bold mb-2 font-sultann">مذكرة داخلية</p>
         @endif
@@ -81,7 +74,7 @@
         <p class="font-sultanb text-xl">تحية طيبة وبعد،،،</p>
 
         <div
-            class="mt-5 pb-5 w-fit max-w-[75%] mx-auto text-center font-sultanb font-bold text-xl border-b border-black">
+            class="mt-5 pb-3 w-fit max-w-[75%] mx-auto text-center font-sultanb font-bold text-xl border-b border-black">
             <span class="whitespace-pre-line">{!! $letter->subject !!}</span>
         </div>
 
@@ -94,19 +87,25 @@
         <div class=" mt-5 pb-16 font-sultanb text-2xl w-2/6 text-center ms-auto">
             {!! $letter->sender !!}</div>
 
-        <pre class="mt-auto"></pre>
-        <div class="text-xs font-sultann">
-            @if ($letter->code)
-                <p>كود ترميز القطاع (15000)</p>
-            @endif
-            @if ($letter->has_attachments)
-                <p>المرفقات: كما ورد أعلاه</p>
-            @endif
+    </div>
+
+    <div class="text-xs font-sultann mx-20 mt-auto">
+        @if ($letter->code)
+            <p>كود ترميز القطاع (15000)</p>
+        @endif
+        @if ($letter->has_attachments)
+            <p>المرفقات: كما ورد أعلاه</p>
+        @endif
+        @if (!$cover)
             <p>نسخة لكل من:</p>
             <pre class=" ms-5 font-sultann">{!! $letter->copyTo !!}</pre>
-            <p class=" ms-5 font-sultann">الملف ({{$letter->id}})</p>
-        </div>
+        @endif
     </div>
+
 </body>
+
+<script>
+    // window.print();
+</script>
 
 </html>
